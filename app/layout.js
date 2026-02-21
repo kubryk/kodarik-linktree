bc1b479535ecbed7ee90d034c01badb3fe98a68d
kubryk <flock1715@gmail.com>
2026-02-21 23:35:03 +0200

    add analytic, insights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   subsets: ["latin", "cyrillic"],
@@ -22,6 +24,8 @@ export default function RootLayout({ children }) {
     <html lang="uk">
       <body className={`${inter.variable} ${jetbrainsMono.variable}`}>
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
